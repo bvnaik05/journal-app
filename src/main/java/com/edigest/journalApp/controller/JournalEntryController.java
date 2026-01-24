e81c1fea5a1ef37c943335b47077ad0ffd437350
Bhargavi Naik <168259289+bvnaik05@users.noreply.github.com>
2026-01-24 20:32:46 +0530

    Added GetMapping and PostMapping to /jounal

--- src/main/java/com/edigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/edigest/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,27 @@
 package com.edigest.journalApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.edigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getaAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
 }
